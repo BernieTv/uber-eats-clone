c4ca32146ecde0f1b7bca9b634d702206b61c79e
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-11-20 14:01:19 +0500

    creating orders module

--- uber-eats-backend/src/restaurants/restaurants.module.ts
+++ uber-eats-backend/src/restaurants/restaurants.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
+import { Dish } from './entities/dish.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.repository';
-import { CategoryResolver, RestaurantsResolver } from './restaurants.resolver';
+import {
+	CategoryResolver,
+	DishResolver,
+	RestaurantsResolver,
+} from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Restaurant, Category])],
+	imports: [TypeOrmModule.forFeature([Restaurant, Dish, Category])],
 	providers: [
 		RestaurantsResolver,
 		CategoryResolver,
+		DishResolver,
 		RestaurantService,
 		CategoryRepository,
 	],
